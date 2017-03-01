5deeb716395de9c49960cea244db419563414eb7
YourName <you@exampl.com>
2017-03-01 09:31:32 +0100

    commit pour tp04

--- src/app.module.js
+++ src/app.module.js
@@ -1,3 +1,26 @@
-(function(){
-	const app = angular.module('rhapp', []);
+(function() {
+    const app = angular.module('rhapp', [
+        'ngRoute',
+        'rhapp.about',
+        'rhapp.home',
+        'rhapp.news'
+        ]);
+
+    app.config(function($routeProvider, $locationProvider){
+        $locationProvider.html5Mode(true);
+
+        $routeProvider
+        .when('/home', {
+            templateUrl:'src/home/home.part.html',
+            controller:'HomeController as hCtrl'
+        })
+        .when('/about', {
+            templateUrl:'src/about/about.part.html'
+        })
+
+        .otherwise('/home');
+
+    });
 })();
+
+
